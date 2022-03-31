be784091f207ba5b4bc1d40c0277b99f46695ae1
P100Den <denis_fantom@mail.ru>
2022-03-31 14:10:06 +0300

    Initial commit

---
+++ src/main/java/test/gradle/APP.java
@@ -0,0 +1,8 @@
+package test.gradle;
+import org.joda.time.LocalTime;
+public class APP {
+    public static void main(String[] args) {
+        LocalTime currentTime = new LocalTime();
+        System.out.println("The current local time is: " + currentTime);
+    }
+}
